5a1e3e3e17d584df9bdc767f93f9767a05930be3
gttffv <66701766+gttffv@users.noreply.github.com>
2020-07-01 21:33:26 +0300

    تم حل التمرين

---
+++ Development/js/scripts.js
@@ -0,0 +1,27 @@
+function logger(array){
+for ( let friends of Array){
+   console.log(friends);
+}
+let friends = ["abduolrahman","abdulla"];
+logger(friends);
+}
+
+logger();
+let degrees = [30,43,50]
+function hottesdays(ph,namber){
+  for(let degree of degrees)
+    if (ph > namber){
+       console.log(ph)
+       
+    }
+
+}
+
+hottesdays(deugger , 30);
+
+
+function getBookById(bookId, books){
+
+ let book = document.getElementById("bookId").Value;
+  Document.getElementById("books").jsdeveloperHTML=book;
+}
